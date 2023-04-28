92f312d49f39339f9d991ce272d45c7af4dba91d
Sheryar Ahmed <royalsheryar505@gmail.com>
2023-04-28 18:54:24 +0500

    add admin-roles to routes closed #13

--- backend/routes/routeProducts.js
+++ backend/routes/routeProducts.js
@@ -6,14 +6,14 @@ const {
     deleteProduct,
     productDetails
 } = require('../controllers/productController');
-const {isAuthenticated, authorizeRole} = require('../middleware/authmiddleware');
+const { isAuthenticated, authorizeRole } = require('../middleware/authmiddleware');
 const router = express.Router();
 
-router.route('/products').get(isAuthenticated,authorizeRole("admin"),getAllProduct);
-router.route('/products/new').post(isAuthenticated, creatProduct);
+router.route('/products').get(getAllProduct);
+router.route('/products/new').post(isAuthenticated, authorizeRole("admin"), creatProduct);
 router.route('/products/:id')
-    .put(isAuthenticated, updateProduct)
-    .delete(isAuthenticated, deleteProduct)
+    .put(isAuthenticated, authorizeRole("admin"), updateProduct)
+    .delete(isAuthenticated, authorizeRole("admin"), deleteProduct)
     .get(productDetails);
 
 module.exports = router;
