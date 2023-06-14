c398b82a6c28818413d40fdea8696e369e44dcee
royalsheryar505@gmail.com <royalsheryar505@gmail.com>
2023-06-14 15:36:29 +0500

    created different folders for backend and frontend Closed #158

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: `${process.env.REACT_APP_BASE_URL}`,
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
